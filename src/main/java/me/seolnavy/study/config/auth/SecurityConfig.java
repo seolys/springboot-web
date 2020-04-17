c5eaa0872abcba5b7d9ca0d980b94c2f40523f62
seol <>
2020-04-17 22:30:48 +0900

    ProfileController 추가

--- src/main/java/me/seolnavy/study/config/auth/SecurityConfig.java
+++ src/main/java/me/seolnavy/study/config/auth/SecurityConfig.java
@@ -14,35 +14,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http
-//            .csrf().disable()
-//            .headers().frameOptions().disable()
-//            .and()
-//                .authorizeRequests()
-//                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
-//                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-//                .anyRequest().authenticated()
-//            .and()
-//                .logout().logoutSuccessUrl("/")
-//            .and()
-//                .oauth2Login()
-//                .userInfoEndpoint()
-//                .userService(customOAuth2UserService);
         http
-                .csrf().disable()
-                .headers().frameOptions().disable()
-                .and()
-                .authorizeRequests()
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
-                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                .anyRequest().authenticated()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/")
-                .and()
-                .oauth2Login()
-                .userInfoEndpoint()
-                .userService(customOAuth2UserService);
+            .csrf().disable()
+            .headers().frameOptions().disable()
+            .and()
+            .authorizeRequests()
+            .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+            .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+            .anyRequest().authenticated()
+            .and()
+            .logout()
+            .logoutSuccessUrl("/")
+            .and()
+            .oauth2Login()
+            .userInfoEndpoint()
+            .userService(customOAuth2UserService);
 
     }
 }
