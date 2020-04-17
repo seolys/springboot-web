d6edd37e15d1ae7b598975c9e11eccd1b4541ec8
seol <seolnavy@naver.com>
2020-04-18 00:08:29 +0900

    무중단 배포를 쉘스크립트 추가.

--- bin/step3/health.sh
+++ bin/step3/health.sh
@@ -12,7 +12,7 @@ echo "> IDLE_PORT: $IDLE_PORT"
 echo "> curl -s http://localhost:$IDLE_PORT/profile"
 sleep 10
 
-for RETRY_COUNT in {1..10}
+for RETRY_COUNT in {1..20}
 do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -1)
@@ -27,13 +27,13 @@ do
     echo "> Health check: ${RESPONSE}"
   fi
 
-  if [ ${RETRY_COUNT} -eq 10 ]
+  if [ ${RETRY_COUNT} -eq 20 ]
   then
     echo "> Health check 실패."
-    echo "> 엔진엑스에 견결하지 않고 배포를 종료합니다."
+    echo "> 엔진엑스에 결하지 않고 배포를 종료합니다."
     exit 1
   fi
 
-  echo "> Health check 연결 실패. 재시도.."
+  echo "> ${RETRY_COUNT} - Health check 연결 실패. 재시도.."
   sleep 10
 done
