6ab0796ba5abec1c0ce85568047ddf2a224dbd85
seol <seolnavy@naver.com>
2020-04-18 18:51:13 +0900

    jUnit test 통과하도록 수정.

--- src/main/java/me/seolnavy/study/Application.java
+++ src/main/java/me/seolnavy/study/Application.java
@@ -1,9 +1,17 @@
 package me.seolnavy.study;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-
+@Slf4j
 @SpringBootApplication
 public class Application {
 
@@ -11,10 +19,22 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-//    @Bean
-//    public ServletRegistrationBean h2servletRegistration() {
-//        ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
-//        registration.addUrlMappings("/h2-console/*");
-//        return registration;
-//    }
+    @Bean
+    public BasicAWSCredentials awsCredentials(
+            @Value("${AWS_ACCESS_KEY}") String accessKey,
+            @Value("${AWS_SECRET_KEY}") String secretKey
+    ) {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+        return awsCreds;
+    }
+
+    @Bean
+    public AmazonS3 awsS3Client(BasicAWSCredentials basicAWSCredentials) {
+        AmazonS3 s3Builder = AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.AP_NORTHEAST_2)
+                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+                .build();
+        return s3Builder;
+    }
+
 }
